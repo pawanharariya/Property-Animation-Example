6ffd1c56be24d5091af6120702f150f5186a2c00
pawanharariya <pawanharariya@gmail.com>
2023-09-18 00:34:59 +0530

    Add translation, scaling and fading animation

--- app/src/main/java/com/example/propertyanimationexample/MainActivity.kt
+++ app/src/main/java/com/example/propertyanimationexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.propertyanimationexample
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -56,30 +57,73 @@ class MainActivity : AppCompatActivity() {
     private fun rotater() {
         val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
         animator.duration = 2000
-        animator.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationStart(animation: Animator) {
-                rotateButton.isEnabled = false
-            }
-
-            override fun onAnimationEnd(animation: Animator) {
-                rotateButton.isEnabled = true
-            }
-        })
+        animator.disableViewDuringAnimation(rotateButton)
         animator.start()
     }
 
     private fun translater() {
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
+        animator.duration = 1000
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(translateButton)
+        animator.start()
     }
 
     private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(star, scaleX, scaleY)
+        animator.duration = 1000
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(scaleButton)
+        animator.start()
     }
 
     private fun fader() {
+        val animator = ObjectAnimator.ofFloat(star, View.ALPHA, 0f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(fadeButton)
+        animator.start()
     }
 
     private fun colorizer() {
     }
 
     private fun shower() {
+    }
+
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                view.isEnabled = true
+            }
+        })
+    }
+
+    private fun combinedAnimation() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+        val translateX = PropertyValuesHolder.ofFloat(View.TRANSLATION_X, 200f)
+        val rotate = PropertyValuesHolder.ofFloat(View.ROTATION, -360f, 0f)
+        val fade = PropertyValuesHolder.ofFloat(View.ALPHA, 0f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(
+            star,
+            scaleX,
+            scaleY,
+            translateX,
+            rotate,
+            fade
+        )
+        animator.duration = 2000
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.start()
     }
 }
